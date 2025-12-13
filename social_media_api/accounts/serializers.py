e01e57a173bc8910015ea2a0429645072852500a
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-12-13 11:28:31 +0100

    set up social media API with custom user auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,14 +2,12 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
