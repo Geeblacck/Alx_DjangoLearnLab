5b91cbcfe321d4a7de9dff37a49e01b01a07e637
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-12-13 11:45:04 +0100

    Remove invalid import and add UserProfileSerializer to serializers.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+"""
+URL configuration for social_media_api project.
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+The `urlpatterns` list routes URLs to views.
+"""
 
 from django.contrib import admin
 from django.urls import path, include
@@ -27,4 +26,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
 ]
