9ccc7a702d20a1fa682e8ed0ad4976e7ede51dc4
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-12-13 11:55:44 +0100

    Add user follow system and feed endpoint

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -25,14 +25,13 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]  # <-- required by ALX
 
     def get(self, request):
         # Get posts from users the current user follows
@@ -40,3 +39,4 @@ class FeedView(APIView):
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
