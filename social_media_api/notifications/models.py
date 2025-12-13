268bb71460818d3e0227e3e42407f501c6c1b2c5
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-12-13 12:07:47 +0100

    Implement Likes and Notifications functionality with API endpoints

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from accounts.models import CustomUser
 
-# Create your models here.
+class Notification(models.Model):
+    recipient = models.ForeignKey(CustomUser, related_name='notifications', on_delete=models.CASCADE)
+    actor = models.ForeignKey(CustomUser, related_name='actions', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(blank=True, null=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} {self.target}"
